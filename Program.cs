35381398746550f7d96264d22206b68d0bd40145
thipwaree <thipwareeaunruean@MacBook-Air-khxng-Thipwaree.local>
2023-03-13 16:46:22 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class program{
+    static void Main (string[] args){
+    
+         Console.WriteLine("day of week");
+            string day = Console.ReadLine();
+               if (int.TryParse("day of week", out int number) && number >= 0 && number <=7) //ยอมแพ้แล้วค่าาาาาาาา
+
+        Console.WriteLine("time of day");    //ฮือออออออออออออออออ
+                string time = Console.ReadLine();
+                if (int.TryParse("time of day", out int n) && n >= 8 && n <=18){
+
+                       Console.WriteLine("menu");
+                           string menu = Console.ReadLine();
+
+                }
+      
+            
+            }
+                
+    }
+  
+
+
+
+
+
+
+
+    
+
+
+    
+
